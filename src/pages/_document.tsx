a2512651865dfbb185d2525aa872ef35fa9aeb85
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-14 20:31:13 -0300

    Configurando Stitches [Estilização a partir do SSR]

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,4 +1,5 @@
 import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '../styles'
 
 export default function Document() {
   return (
@@ -8,6 +9,13 @@ export default function Document() {
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap" rel="stylesheet" />
+
+        {/*
+          Faz com que o css seja renderizado também com SSR.
+          getCssText(): Antes de renderizar a página, irá identificar todo o código css
+          necessitado pela página e o escreverá dentro de uma tag 'style'.
+        */}
+        <style id='stitches' dangerouslySetInnerHTML={{ __html: getCssText() }} />
       </Head>
       <body>
         <Main /> {/* O Main seria mais ou menos a <div id="root"></div> em outras aplicações ReactJS */}
