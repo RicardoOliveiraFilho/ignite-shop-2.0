0600b54760719f0cbda8fba03488e48ba5d37e8c
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-17 19:59:04 -0300

    Estrutura da página de produto

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,7 +1,23 @@
 import { useRouter } from "next/router"
+import { ImageContainer, ProductContainer, ProductDetails } from "../../styles/pages/product"
 
 export default function Product() {
   const { query } = useRouter()
 
-  return <h1>Página do Produto {JSON.stringify(query)}</h1>
+  return (
+    <ProductContainer>
+      <ImageContainer></ImageContainer>
+
+      <ProductDetails>
+        <h1>Camiseta X</h1>
+        <span>R$ 79,90</span>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui, omnis consequuntur! Dolores deleniti consectetur atque dolore quas voluptatum iure temporibus enim itaque libero corporis accusamus, velit aliquam rerum esse et!</p>
+
+        <button>
+            Comprar Agora
+        </button>
+      </ProductDetails>
+    </ProductContainer>
+  )
 }
