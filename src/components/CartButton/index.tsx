7203a8efd1ad650a9358cb4c03c6526b463cd8b9
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-19 15:36:05 -0300

    [DESAFIO IV]: Implementação inicial do Dialog utilizando o Radix

--- src/components/CartButton/index.tsx
+++ src/components/CartButton/index.tsx
@@ -1,9 +1,13 @@
 import { Handbag } from 'phosphor-react'
+import { ComponentProps } from 'react';
 import { CartButtonContainer } from "./styles";
 
-export function CartButton() {
+// Dizemos que o nosso cart button terá todas as propriedades que existem em CartButtonContainer.
+type CartButtonProps = ComponentProps<typeof CartButtonContainer>
+
+export function CartButton({ ...rest }: CartButtonProps) { // '...rest': Propriedades passadas pelo Radix!
   return (
-    <CartButtonContainer>
+    <CartButtonContainer {...rest}>
       <Handbag weight='bold' />
     </CartButtonContainer>
   )
