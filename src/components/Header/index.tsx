77161451e61addd14409074d287d4c2610727bc6
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-19 15:08:57 -0300

    [DESAFIO IV]: Adicionando o componente de Carrinho e o seu componente de Botão

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,6 +2,7 @@ import Link from "next/link";
 import Image from "next/future/image"
 import { HeaderContainer } from './styles'
 import logoImg from '../../assets/logo.svg'
+import { Cart } from "../Cart";
 
 export function Header() {
   return (
@@ -9,6 +10,8 @@ export function Header() {
       <Link href="/">
         <Image src={logoImg} alt="" />
       </Link>
+
+      <Cart />
     </HeaderContainer>
   )
 }
