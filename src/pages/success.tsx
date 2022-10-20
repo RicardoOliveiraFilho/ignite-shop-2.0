4bf08797c3b130ee6f9d818e4316ea340830d23f
Ricardo Oliveira <ricardo.oliveira1307@gmail.com>
2022-10-20 15:58:22 -0300

    [DESAFIO IV]: Ajustando a página de sucesso

--- src/pages/success.tsx
+++ src/pages/success.tsx
@@ -4,17 +4,14 @@ import Head from "next/head";
 import Link from "next/link";
 import Stripe from "stripe";
 import { stripe } from "../lib/stripe";
-import { ImageContainer, SuccessContainer } from "../styles/pages/success";
+import { ImageContainer, ImagesContainer, SuccessContainer } from "../styles/pages/success";
 
 interface SuccessProps {
   customerName: string
-  product: {
-    name: string
-    imageUrl: string
-  }
+  productsImages: string[]
 }
 
-export default function Success({ customerName, product }: SuccessProps) {
+export default function Success({ customerName, productsImages }: SuccessProps) {
   return (
     <>
       <Head>
@@ -25,14 +22,19 @@ export default function Success({ customerName, product }: SuccessProps) {
       </Head>
 
       <SuccessContainer>
+        <ImagesContainer>
+          {productsImages.map(image => (
+            <ImageContainer key={image}>
+              <Image src={image} width={120} height={110} alt="" />
+            </ImageContainer>
+          ))}
+        </ImagesContainer>
+
         <h1>Compra efetuada!</h1>
 
-        <ImageContainer>
-          <Image src={product.imageUrl} width={120} height={110} alt="" />
-        </ImageContainer>
-
         <p>
-          Uhuul <strong>{customerName}</strong>, sua <strong>{product.name}</strong> já está a caminho da sua casa.
+          Uhuul <strong>{customerName}</strong>, sua compra de{" "}
+          {productsImages.length >= 2 ? `${productsImages.length} camisetas` : `${productsImages.length} camiseta`} já está a caminho da sua casa.
         </p>
 
         {/* A partir da versão 12 do Next.js não é mais necessário o componente 'Link' ter o elemento 'a' como filho. */}
@@ -63,17 +65,15 @@ export const getServerSideProps: GetServerSideProps = async ({ query }) => {
   })
 
   const customerName = session.customer_details.name
-
-  // Como o expandimos o produto, devemos indicar esse seu tipo!
-  const product = session.line_items.data[0].price.product as Stripe.Product
+  const productsImages = session.line_items.data.map((item) => {
+    const product = item.price.product as Stripe.Product
+    return product.images[0]
+  })
 
   return {
     props: {
       customerName,
-      product: {
-        name: product.name,
-        imageUrl: product.images[0] // Array de string com os paths das imagens!
-      }
+      productsImages,
     }
   }
 }
